6bf6157c024541e4583165913e140ecff16f992a
dovneko <dovneko@users.noreply.github.com>
2016-08-06 18:51:59 +0900

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,18 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/test", te)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "２回目")
+}
+func te(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "kame")
+}
